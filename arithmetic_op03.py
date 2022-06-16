e09985666c4ec9c14062e7ba43c85ebb50d9afe1
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-06-16 09:16:54 +0500

    3

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -3,3 +3,6 @@
 #Find the reverse of the number and assign it to a variable called 'answer'.
 
 #Print the answer variable
+number = 28
+answer = (number % 10) * 10 + (number -(number % 10))//10
+print(answer)
